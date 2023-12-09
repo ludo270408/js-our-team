ed3096353f5cc896187b8521bf1bc3013937f131
ludo270408 <ludovica.sette7@gmaio.com>
2023-12-09 22:28:11 +0100

    [UPTADE]  Problem solved

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 //MILESTONE 0: Creare l’array di oggetti con le informazioni fornite
  const team_members = [
     {
-        name: Wayne Barnett;
-        role: Founder & CEO;
-        image: wayne-barnett-founder-ceo.jpg;
+        name: 'Wayne Barnett';
+        role: 'Founder & CEO';
+        image: 'wayne-barnett-founder-ceo.jpg';
     }
     {
-        name: Angela Caroll;
-        role: Chief Editor;
-        image: angela-caroll-chief-editor.jpg;
+        name: 'Angela Caroll';
+        role: 'Chief Editor';
+        image: 'angela-caroll-chief-editor.jpg';
     }
     {
-        name: Walter Gordon;
-        role: Office Manager;
-        image: walter-gordon-office-manager.jpg;
+        name: 'Walter Gordon';
+        role: 'Office Manager';
+        image: 'walter-gordon-office-manager.jpg';
     }
     {
-        name: Angela Lopez;
-        role: Social Media Manager;
-        image: angela-lopez-social-media-manager.jpg;
+        name: 'Angela Lopez';
+        role: 'Social Media Manager';
+        image: 'angela-lopez-social-media-manager.jpg';
     }
     {
-        name: Scott Estrada;
-        role: Developer;
-        image: scott-estrada-developer.jpg;
+        name: 'Scott Estrada';
+        role: 'Developer';
+        image: 'scott-estrada-developer.jpg';
     }
     {
-        name: Barbara Ramos;
-        role: Graphic Designer;
-        image: barbara-ramos-graphic-designer.jpg;
+        name: 'Barbara Ramos';
+        role: 'Graphic Designer';
+        image: 'barbara-ramos-graphic-designer.jpg';
     }
  ];
 
@@ -36,3 +36,5 @@
   for(let i=0; i<team_members.length; i++){
     console.log(team_members[i]);
   }
+
+  //MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
